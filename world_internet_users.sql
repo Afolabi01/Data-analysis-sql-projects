c3e3f0ab2bcd49a3fe525fa54c5405a01e68e3d5
Afolabi01 <feerrup01@gmail.com>
2022-12-25 16:03:04 +0100

    The Statistics of Internet users
    A project to look at the world internet users broken down by country
    and region(continent).An aggregation look and some other inferences.

---
+++ world_internet_users.sql
@@ -0,0 +1,43 @@
+/*
+CREATE TABLE IF NOT EXISTS world_internet_users (
+   country VARCHAR(100),
+   region VARCHAR(100),
+   population BIGINT,
+   internet_users BIGINT,
+   percentage_of_population DECIMAL(20)
+)
+
+COPY world_internet_users(country,region,population,internet_users,percentage_of_population)
+FROM 'C:\Users\Public\world_internet_user.csv'
+DELIMITER ','
+CSV HEADER;
+*/
+
+--Checking if observations were copied correctly
+SELECT *
+FROM world_internet_users;
+
+--Confirmed our data is complete and we have 243 rows including the world statistics
+--Movong forward I would be excluding the world statistics from most of my questions
+
+--What are the population of users per region?
+SELECT region, SUM(internet_users) as internet_user_by_region , SUM(population) as region_population
+FROM world_internet_users
+WHERE country <> 'World'
+GROUP BY region
+ORDER BY internet_user_by_region DESC;
+
+--What percentage of each region are internet users
+SELECT region, ROUND((internet_user_by_region / region_population) *100,2) as percentage_internet_users_by_region
+FROM (SELECT region, SUM(internet_users) as internet_user_by_region , SUM(population) as region_population
+             FROM world_internet_users
+             WHERE country <> 'World'
+             GROUP BY region) as region_stat
+ORDER BY percentage_internet_users_by_region DESC;
+
+--What country had the highest amount of internet users for each region
+WITH max_values as (SELECT country , region , MAX(internet_users)
+FROM world_internet_users
+WHERE country <> 'Africa'
+GROUP BY country ,region
+ORDER BY region;)
